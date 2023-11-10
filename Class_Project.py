80d293e840f1614a717b5e011f35b48432f48d38
Gear-change <speedmanjm@gmail.com>
2023-11-09 19:07:30 -0500

    finished merge
    Added new field for selecting and deselecting degrees, fixed bug with how values were getting stored and such.

--- Class_Project.py
+++ Class_Project.py
@@ -1,7 +1,20 @@
-#https://www.geeksforgeeks.org/creating-tabbed-widget-with-python-tkinter/
-
 import tkinter as tk                     
 from tkinter import Entry, Grid, ttk 
+
+def degreeToString(degree):
+    try:
+        stringOut = degree.get("degreeType") + " in " + degree.get("degreeField") + ":" +("degreeSubField")
+        return stringOut
+    except:
+        print("error degree")
+        print(degree)
+        return " "
+    
+global tab2
+tab2 = None 
+global DegreeFrame
+DegreeFrame = tk.Frame(tab2)
+
 def addNewDegree(DegreeType, DegreeField, degreeSubField,schoolAddress1, schoolCity, schoolAddress2, schoolState, SchoolDateEndMonth, SchoolDateEndYear, SchoolDateStartMonth, SchoolDateStartYear,GPA):
     global userEducation
     global genericEducation
@@ -18,8 +31,19 @@ def addNewDegree(DegreeType, DegreeField, degreeSubField,schoolAddress1, schoolC
     newEducation["dateStartMonth"] = SchoolDateStartMonth
     newEducation["dateEndYear"] = SchoolDateEndYear
     newEducation["dateEndMonth"] = SchoolDateEndMonth
+    userEducation.append(newEducation)
+    global DegreeFrame
+    degreeNum = len(userEducation) - 1
+    newCheckbox = tk.Checkbutton(DegreeFrame, text=degreeToString(newEducation),
+                                     variable=userEducation[degreeNum], 
+                                     onvalue=True,
+                                     offvalue=False)
+    newCheckbox.grid(column=0,row=degreeNum)
+    newButton = tk.Button(DegreeFrame, text="delete degree", command=(userEducation.pop(degreeNum)))
+    newButton.grid(column=1, row=degreeNum)
 
 def mainApp():
+    global userEducation
     global firstName  
     global lastName
     global userAddressLine1
@@ -28,10 +52,11 @@ def mainApp():
     global userState
     global userEmail
     global userDescription
+    #https://www.geeksforgeeks.org/creating-tabbed-widget-with-python-tkinter/
     root = tk.Tk() 
     root.title("Resume Generator") 
     tabControl = ttk.Notebook(root) 
-  
+    global tab2
     tab1 = ttk.Frame(tabControl) 
     tab2 = ttk.Frame(tabControl) 
     tab3 = ttk.Frame(tabControl)
@@ -78,65 +103,170 @@ def mainApp():
     ttk.Label(tab2,text = "Degree type:").grid(column=currCol,row=currRow)
     currCol += 1
     #thanks:https://www.geeksforgeeks.org/combobox-widget-in-tkinter-python/?ref=lbp
-    DegreeType = ttk.Combobox(tab2, width = 27, textvariable = tk.StringVar() , values = [ ' Highschool',  ' Accociate', ' Bachelor', ' Master', ' Doctorate' ]).grid(column=currCol,row=currRow, columnspan=3)
+    DegreeType = tk.StringVar()
+    newcombo = ttk.Combobox(tab2, width = 27, textvariable = DegreeType , values = [ ' Highschool',  ' Accociate', ' Bachelor', ' Master', ' Doctorate' ])
+    newcombo.grid(column=currCol,row=currRow, columnspan=3)
     currCol+=3
     ttk.Label(tab2, text="Degree Field: ").grid(column=currCol,row=currRow)
     currCol+=1
-    DegreeField = Entry(tab2).grid(column=currCol,row=currRow)
+    DegreeField = tk.StringVar()
+    newEntry = Entry(tab2, width = 27, textvariable=DegreeField )
+    newEntry.grid(column=currCol,row=currRow)
     currCol = 4
     currRow +=1
     ttk.Label(tab2, text="Degree Subfield: ").grid(column=currCol,row=currRow)
     currCol+=1
-    degreeSubField = Entry(tab2).grid(column=currCol,row=currRow)
+    degreeSubField = tk.StringVar()
+    newEntry = Entry(tab2, width = 27, textvariable=degreeSubField )
+    newEntry.grid(column=currCol,row=currRow)
     currCol = 0
     currRow += 1
     ttk.Label(tab2, text="Address of School: ").grid(column=currCol,row=currRow)
     currCol+=1
-    schoolAddress1 = Entry(tab2).grid(column=currCol,row=currRow,columnspan=3)
+    schoolAddress1 = tk.StringVar()
+    newEntry = Entry(tab2, width = 27, textvariable=schoolAddress1 )
+    newEntry.grid(column=currCol,row=currRow,columnspan=3)
     currCol+=3
     ttk.Label(tab2, text="School City").grid(column=currCol,row=currRow)
     currCol+=1
-    schoolCity = Entry(tab2).grid(column=currCol,row=currRow)
+    schoolCity = tk.StringVar()
+    newEntry = Entry(tab2, width = 27, textvariable=schoolCity )
+    newEntry.grid(column=currCol,row=currRow)
     currCol = 0
     currRow+=1
     ttk.Label(tab2, text="Address line 2:").grid(column=currCol,row=currRow)
     currCol+=1
-    schoolAddress2 = Entry(tab2).grid(column=currCol,row=currRow,columnspan=3)
+    schoolAddress2 = tk.StringVar()
+    newEntry = Entry(tab2, width = 27, textvariable=schoolAddress2 )
+    newEntry.grid(column=currCol,row=currRow,columnspan=3)
     currCol+=3
     ttk.Label(tab2, text="School State: ").grid(column=currCol,row=currRow)
     currCol+=1
-    schoolState = ttk.Combobox(tab2, width = 27, textvariable = tk.StringVar() , values = [ 'Alabama', 'Alaska', 'Arizona', 'Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']).grid(column=currCol,row=currRow)
+    schoolState = tk.StringVar()
+    newComboBox = ttk.Combobox(tab2, width = 27, textvariable = schoolState , values = [ 'Alabama',
+                                                                                            'Alaska', 
+                                                                                            'Arizona', 
+                                                                                            'Arkansas',
+                                                                                            'California',
+                                                                                            'Colorado',
+                                                                                            'Connecticut',
+                                                                                            'Delaware',
+                                                                                            'Florida',
+                                                                                            'Georgia',
+                                                                                            'Hawaii',
+                                                                                            'Idaho',
+                                                                                            'Illinois',
+                                                                                            'Indiana',
+                                                                                            'Iowa',
+                                                                                            'Kansas',
+                                                                                            'Kentucky',
+                                                                                            'Louisiana',
+                                                                                            'Maine',
+                                                                                            'Maryland',
+                                                                                            'Massachusetts',
+                                                                                            'Michigan',
+                                                                                            'Minnesota',
+                                                                                            'Mississippi',
+                                                                                            'Missouri',
+                                                                                            'Montana',
+                                                                                            'Nebraska',
+                                                                                            'Nevada',
+                                                                                            'New Hampshire',
+                                                                                            'New Jersey',
+                                                                                            'New Mexico',
+                                                                                            'New York',
+                                                                                            'North Carolina',
+                                                                                            'North Dakota',
+                                                                                            'Ohio',
+                                                                                            'Oklahoma',
+                                                                                            'Oregon',
+                                                                                            'Pennsylvania',
+                                                                                            'Rhode Island',
+                                                                                            'South Carolina',
+                                                                                            'South Dakota',
+                                                                                            'Tennessee',
+                                                                                            'Texas',
+                                                                                            'Utah',
+                                                                                            'Vermont',
+                                                                                            'Virginia',
+                                                                                            'Washington',
+                                                                                            'West Virginia',
+                                                                                            'Wisconsin',
+                                                                                            'Wyoming'
+                                                                                            ])
+    newComboBox.grid(column=currCol,row=currRow)
     currRow += 1
     currCol = 0
     ttk.Label(tab2, text="Graduation date: ").grid(column=currCol,row=currRow)
     currCol += 1
     #date frame construct
-    SchoolDateEndMonth = ttk.Combobox(tab2, width = 5, textvariable = tk.StringVar() , values = ['1','2','3','4','5','6','7','8','9','10','11','12']).grid(column=currCol,row=currRow)
+    SchoolDateEndMonth = tk.IntVar()
+    newComboBox = ttk.Combobox(tab2, width = 5, textvariable = SchoolDateEndMonth , values = [1,2,3,4,5,6,7,8,9,10,11,12])
+    newComboBox.grid(column=currCol,row=currRow)
     yearsList= []
     currCol +=1
     ttk.Label(tab2, text=" - ").grid(column=currCol,row=currRow)
     for x in range(1950,2050):
-        yearsList.append(str(x))      
+        yearsList.append(x)      
     currCol +=1  
-    SchoolDateEndYear = ttk.Combobox(tab2, width = 5, textvariable = tk.StringVar() , values = yearsList).grid(column=currCol,row=currRow)
+    SchoolDateEndYear = tk.IntVar()
+    newComboBox = ttk.Combobox(tab2, width = 5, textvariable = SchoolDateEndYear , values = yearsList)
+    newComboBox.grid(column=currCol,row=currRow)
     currCol +=1
     ttk.Label(tab2, text="GPA: ").grid(column=currCol, row=currRow)
     currCol +=1
-    GPA = Entry(tab2).grid(column=currCol,row=currRow)
+    GPA = tk.DoubleVar()
+    newEntry = Entry(tab2, width = 27, textvariable = GPA )
+    newEntry.grid(column=currCol,row=currRow)
     #date frame construct
     currRow += 1
     currCol = 0
     ttk.Label(tab2, text="Start date: ").grid(column=currCol,row=currRow)
     currCol +=1
-    SchoolDateStartMonth = ttk.Combobox(tab2, width = 5, textvariable = tk.StringVar() , values = ['1','2','3','4','5','6','7','8','9','10','11','12']).grid(column=currCol,row=currRow)
+    SchoolDateStartMonth = tk.IntVar()
+    newComboBox = ttk.Combobox(tab2, width = 5, textvariable = SchoolDateStartMonth , values = [1,2,3,4,5,6,7,8,9,10,11,12])
+    newComboBox.grid(column=currCol,row=currRow)
     currCol +=1
     ttk.Label(tab2, text=" - ").grid(column=currCol,row=currRow)
     currCol +=1
-    SchoolDateStartYear = ttk.Combobox(tab2, width = 5, textvariable = tk.StringVar(), values = yearsList).grid(column=currCol,row=currRow)
+    SchoolDateStartYear = tk.IntVar()
+    newComboBox = ttk.Combobox(tab2, width = 5, textvariable = SchoolDateStartYear, values = yearsList)
+    newComboBox.grid(column=currCol,row=currRow)
     #add the button to call the function to add an education
     currCol +=2
-    btnSchoolSubmit = tk.Button(tab2, text="add Degree", command=addNewDegree(DegreeType, DegreeField, degreeSubField,schoolAddress1, schoolCity, schoolAddress2, schoolState, SchoolDateEndMonth, SchoolDateEndYear, SchoolDateStartMonth, SchoolDateStartYear,GPA))
+    btnSchoolSubmit = tk.Button(tab2, 
+                                text="add Degree", 
+                                command=addNewDegree(DegreeType.get(), 
+                                                     DegreeField.get(), 
+                                                     degreeSubField.get(),
+                                                     schoolAddress1.get(), 
+                                                     schoolCity.get(), 
+                                                     schoolAddress2.get(), 
+                                                     schoolState.get(), 
+                                                     SchoolDateEndMonth.get(), 
+                                                     SchoolDateEndYear.get(), 
+                                                     SchoolDateStartMonth.get(),
+                                                     SchoolDateStartYear.get(),
+                                                     GPA.get()))
     btnSchoolSubmit.grid(column=currCol,row=currRow)
+    global DegreeFrame
+    DegreeFrame = tk.Frame(tab2)
+    if (len(userEducation) < 0):
+        currCol =0
+        currRow +=1
+        #now to make the selection interface
+        global degreeNum
+        degreeNum = 0
+        for degree in userEducation:
+            newCheckbox = tk.Checkbutton(DegreeFrame, text=degreeToString(degree),
+                                     variable=userEducation[degreeNum], 
+                                     onvalue=True,
+                                     offvalue=False)
+            newCheckbox.grid(column=0,row=degreeNum)
+            newButton = tk.Button(DegreeFrame, text="delete degree", command=(userEducation.pop(degreeNum)))
+            newButton.grid(column=1, row=degreeNum)
+            degreeNum += 1
+        DegreeFrame.grid(column=currCol,row=currRow,columnspan=5)   
     root.mainloop()   
 global genericEducation
 global genericWork
