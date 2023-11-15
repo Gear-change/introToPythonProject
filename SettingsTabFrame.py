64788ef8388864ba03bdfac5c75f58e99a009d1c
Gear-change <speedmanjm@gmail.com>
2023-11-15 17:09:44 -0500

    Update SettingsTabFrame.py

--- SettingsTabFrame.py
+++ SettingsTabFrame.py
@@ -2,8 +2,18 @@ import tkinter as tk
 from tkinter import ttk
 from CommonTools import *
 
+def saveToFile():
+    
+
 def makeSettingsTab(parent):
     newFrame = tk.Frame(parent)
-    
+    curCol = 0
+    curRow = 0
+    newButton = tk.Button(
+        newFrame,
+        text="save as",
+        command=saveToFile()
+    )
+    newButton.grid(column=curCol, row=curRow)
 
     return newFrame
